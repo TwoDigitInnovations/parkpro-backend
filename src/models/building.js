96eef37b7d6c3f7a79dfc4de72fe8b7f606d7b31
Rishabh-2digit <rishabhtiwari73096@gmail.com>
2026-03-05 10:07:07 +0530

    fix

--- src/models/building.js
+++ src/models/building.js
@@ -2,60 +2,67 @@
 const mongoose = require('mongoose');
 
 const pointSchema = new mongoose.Schema({
-    type: {
-        type: String,
-        enum: ["Point"],
-        default: "Point"
-    },
-    coordinates: {
-        type: [Number],
-        required: true
-    },
+  type: {
+    type: String,
+    enum: ['Point'],
+    default: 'Point',
+  },
+  coordinates: {
+    type: [Number],
+    required: true,
+  },
 });
-
 
 const buildingSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-        },
-        phone_no: {
-            type: String,
-        },
-        contact_no: {
-            type: String,
-        },
-        house_no: {
-            type: String,
-        },
-        street_name: {
-            type: String,
-        },
-        city: {
-            type: String,
-        },
-        postalcode: {
-            type: String,
-        },
-        building_image: {
-            type: String,
-        },
-        location: {
-            type: pointSchema,
-        },
-    }, {
-    timestamps: true
-});
+  {
+    name: {
+      type: String,
+    },
+    building_name: {
+      type: String,
+    },
+    phone_no: {
+      type: String,
+    },
+    contact_no: {
+      type: String,
+    },
+    house_no: {
+      type: String,
+    },
+    street_name: {
+      type: String,
+    },
+    city: {
+      type: String,
+    },
+    postalcode: {
+      type: String,
+    },
+    building_image: {
+      type: String,
+    },
+    address: {
+      type: String,
+    },
+    location: {
+      type: pointSchema,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
 
 buildingSchema.set('toJSON', {
-    getters: true,
-    virtuals: false,
-    transform: (doc, ret, options) => {
-        delete ret.__v;
-        return ret;
-    }
+  getters: true,
+  virtuals: false,
+  transform: (doc, ret, options) => {
+    delete ret.__v;
+    return ret;
+  },
 });
 
-buildingSchema.index({ location: "2dsphere" });
+buildingSchema.index({ location: '2dsphere' });
 
 module.exports = mongoose.model('Building', buildingSchema);
