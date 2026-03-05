2cb828a45034c65eaf8077c496e47a616b7bbcf6
Rishabh-2digit <rishabhtiwari73096@gmail.com>
2026-03-05 16:41:20 +0530

    fix issue

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,7 +2,7 @@ const User = require('@models/User');
 const Verification = require('@models/verification');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const response = require("../responses");
+const response = require('../responses');
 const mailservice = require('@services/mailservice');
 const userHelper = require('../helper/user');
 const moment = require('moment');
@@ -31,7 +31,7 @@ module.exports = {
         return res.status(400).json({ message: 'User already exists' });
       }
       const existingCode = await User.findOne({ code });
-       if (!existingCode) {
+      if (!existingCode) {
         return res.status(400).json({ message: 'Invalid registration code' });
       }
       const hashedPassword = await bcrypt.hash(password, 10);
@@ -40,7 +40,7 @@ module.exports = {
         name,
         email,
         password: hashedPassword,
-        organization:existingCode?._id
+        organization: existingCode?._id,
       });
 
       if (role) {
@@ -55,10 +55,9 @@ module.exports = {
 
       const userResponse = await User.findById(newUser._id).select('-password');
       return response.ok(res, {
-        message: "User registered successfully.",
+        message: 'User registered successfully.',
         data: userResponse,
       });
-
     } catch (error) {
       console.error(error);
       res.status(500).json({ message: 'Server error' });
@@ -66,20 +65,20 @@ module.exports = {
   },
   create_org: async (req, res) => {
     try {
-      const payload= req.body;
+      const payload = req.body;
 
-    let code;
-  let isUnique = false;
+      let code;
+      let isUnique = false;
 
-  while (!isUnique) {
-    // Generates a 6-digit number (100000–999999)
-    code = Math.floor(100000 + Math.random() * 900000).toString();
+      while (!isUnique) {
+        // Generates a 6-digit number (100000–999999)
+        code = Math.floor(100000 + Math.random() * 900000).toString();
 
-    const existingCode = await User.findOne({ code });
-    if (!existingCode) {
-      isUnique = true;
-    }
-  }
+        const existingCode = await User.findOne({ code });
+        if (!existingCode) {
+          isUnique = true;
+        }
+      }
 
       if (payload?.password.length < 6) {
         return res
@@ -87,60 +86,69 @@ module.exports = {
           .json({ message: 'Password must be at least 8 characters long' });
       }
 
-      const existingUser = await User.findOne({ email:payload?.email });
+      const existingUser = await User.findOne({ email: payload?.email });
       if (existingUser) {
         return res.status(400).json({ message: 'User already exists' });
       }
 
       const hashedPassword = await bcrypt.hash(payload?.password, 10);
 
-      let newUser = new User({...payload,role:'admin',password:hashedPassword,code});
+      let newUser = new User({
+        ...payload,
+        role: 'admin',
+        password: hashedPassword,
+        code,
+      });
 
       await newUser.save();
 
       return response.ok(res, {
-        message: "User registered successfully."});
-
+        message: 'User registered successfully.',
+      });
     } catch (error) {
       console.error(error);
       res.status(500).json({ message: 'Server error' });
     }
   },
-  create_landlord : async (req, res) => {
+  create_landlord: async (req, res) => {
     try {
-      const payload= req.body;
-    // let code;
-  // let isUnique = false;
+      const payload = req.body;
+      // let code;
+      // let isUnique = false;
 
-  // while (!isUnique) {
-  //   // Generates a 6-digit number (100000–999999)
-  //   code = Math.floor(100000 + Math.random() * 900000).toString();
+      // while (!isUnique) {
+      //   // Generates a 6-digit number (100000–999999)
+      //   code = Math.floor(100000 + Math.random() * 900000).toString();
 
-  //   const existingCode = await User.findOne({ code });
-  //   if (!existingCode) {
-  //     isUnique = true;
-  //   }
-  // }
+      //   const existingCode = await User.findOne({ code });
+      //   if (!existingCode) {
+      //     isUnique = true;
+      //   }
+      // }
       if (payload?.password.length < 6) {
         return res
           .status(400)
           .json({ message: 'Password must be at least 8 characters long' });
       }
 
-      const existingUser = await User.findOne({ email:payload?.email });
+      const existingUser = await User.findOne({ email: payload?.email });
       if (existingUser) {
         return res.status(400).json({ message: 'User already exists' });
       }
 
       const hashedPassword = await bcrypt.hash(payload?.password, 10);
 
-      let newUser = new User({...payload,role:'landlord',password:hashedPassword});
+      let newUser = new User({
+        ...payload,
+        role: 'landlord',
+        password: hashedPassword,
+      });
 
       await newUser.save();
 
       return response.ok(res, {
-        message: "Landlord registered successfully."});
-
+        message: 'Landlord registered successfully.',
+      });
     } catch (error) {
       console.error(error);
       res.status(500).json({ message: 'Server error' });
@@ -151,23 +159,23 @@ module.exports = {
       const newresponse = await User.findByIdAndUpdate(
         req.body.id,
         { $set: { status: req.body.status } },
-        { new: true }
+        { new: true },
       );
       if (!newresponse) {
-        return response.error(res, "User not found", 404);
+        return response.error(res, 'User not found', 404);
       }
-      if (req.body.status === "VERIFIED") {
+      if (req.body.status === 'VERIFIED') {
         await notify(
           newresponse?._id,
-          "Account Verified",
-          "Your account is now verified",
+          'Account Verified',
+          'Your account is now verified',
         );
       }
-      if (req.body.status === "SUSPEND") {
+      if (req.body.status === 'SUSPEND') {
         await notify(
           newresponse?._id,
-          "Account Suspended",
-          "Your account is suspended",
+          'Account Suspended',
+          'Your account is suspended',
         );
       }
       return response.ok(res, newresponse);
@@ -181,7 +189,9 @@ module.exports = {
       const { email, password } = req.body;
 
       if (!email || !password) {
-        return response.badReq(res, { message: 'Email and password are required' });
+        return response.badReq(res, {
+          message: 'Email and password are required',
+        });
       }
 
       const user = await User.findOne({ email });
@@ -194,7 +204,10 @@ module.exports = {
         return response.unAuthorize(res, { message: 'Invalid credentials' });
       }
 
-      const token = jwt.sign({ id: user._id, email: user.email }, process.env.JWT_SECRET);
+      const token = jwt.sign(
+        { id: user._id, email: user.email },
+        process.env.JWT_SECRET,
+      );
 
       const newData = {
         token,
@@ -203,10 +216,10 @@ module.exports = {
           email: user.email,
           name: user.name,
           role: user.role,
-          image:user.image
+          image: user.image,
         },
       };
-       await Device.updateOne(
+      await Device.updateOne(
         { device_token: req.body.device_token },
         { $set: { player_id: req.body.player_id, user: user._id } },
         { upsert: true },
@@ -224,7 +237,7 @@ module.exports = {
       const user = await User.findOne({ email });
 
       if (!user) {
-        return response.badReq(res, { message: "Email does exist." });
+        return response.badReq(res, { message: 'Email does exist.' });
       }
 
       let ran_otp = Math.floor(1000 + Math.random() * 9000);
@@ -243,7 +256,7 @@ module.exports = {
       // }
       let token = await userHelper.encode(ver._id);
 
-      return response.ok(res, { message: "OTP sent.", token });
+      return response.ok(res, { message: 'OTP sent.', token });
     } catch (error) {
       return response.error(res, error);
     }
@@ -254,7 +267,7 @@ module.exports = {
       const otp = req.body.otp;
       const token = req.body.token;
       if (!(otp && token)) {
-        return response.badReq(res, { message: "otp and token required." });
+        return response.badReq(res, { message: 'otp and token required.' });
       }
       let verId = await userHelper.decode(token);
       let ver = await Verification.findById(verId);
@@ -264,13 +277,13 @@ module.exports = {
         new Date().getTime() < new Date(ver.expiration_at).getTime()
       ) {
         let token = await userHelper.encode(
-          ver._id + ":" + userHelper.getDatewithAddedMinutes(5).getTime()
+          ver._id + ':' + userHelper.getDatewithAddedMinutes(5).getTime(),
         );
         ver.verified = true;
         await ver.save();
-        return response.ok(res, { message: "OTP verified", token });
+        return response.ok(res, { message: 'OTP verified', token });
       } else {
-        return response.notFound(res, { message: "Invalid OTP" });
+        return response.notFound(res, { message: 'Invalid OTP' });
       }
     } catch (error) {
       return response.error(res, error);
@@ -282,23 +295,23 @@ module.exports = {
       const token = req.body.token;
       const password = req.body.password;
       const data = await userHelper.decode(token);
-      const [verID, date] = data.split(":");
+      const [verID, date] = data.split(':');
       if (new Date().getTime() > new Date(date).getTime()) {
-        return response.forbidden(res, { message: "Session expired." });
+        return response.forbidden(res, { message: 'Session expired.' });
       }
       let otp = await Verification.findById(verID);
       if (!otp.verified) {
-        return response.forbidden(res, { message: "unAuthorize" });
+        return response.forbidden(res, { message: 'unAuthorize' });
       }
       let user = await User.findById(otp.user);
       if (!user) {
-        return response.forbidden(res, { message: "unAuthorize" });
+        return response.forbidden(res, { message: 'unAuthorize' });
       }
       await Verification.findByIdAndDelete(verID);
       user.password = user.encryptPassword(password);
       await user.save();
       mailservice.passwordChange({ email: user.email });
-      return response.ok(res, { message: "Password changed! Login now." });
+      return response.ok(res, { message: 'Password changed! Login now.' });
     } catch (error) {
       return response.error(res, error);
     }
@@ -333,73 +346,74 @@ module.exports = {
   getAllUser: async (req, res) => {
     let { page = 1, limit = 20 } = req.query;
 
-      page = parseInt(page);
-      limit = parseInt(limit);
-      
+    page = parseInt(page);
+    limit = parseInt(limit);
+
     try {
       let cond = {
         role: 'user',
       };
 
-      if(req.query.role){
-        cond.role = req.query.role
+      if (req.query.role) {
+        cond.role = req.query.role;
       }
       if (req?.query?.organization) {
         cond.organization = req.user._id;
       }
 
-      let startDate
-      let endDate
+      let startDate;
+      let endDate;
       if (req.query.date) {
         startDate = new Date(req.query.date);
-        console.log('SDDDD', startDate)
-        endDate = new Date(new Date(req.query.date).setDate(startDate.getDate() + 1));
-        console.log('EDDDD', endDate)
+        console.log('SDDDD', startDate);
+        endDate = new Date(
+          new Date(req.query.date).setDate(startDate.getDate() + 1),
+        );
+        console.log('EDDDD', endDate);
         cond.createdAt = { $gte: startDate, $lte: endDate };
       }
 
       if (req.query.key) {
-        cond['$or'] = [
-          { name: { $regex: req.query.key, $options: "i" } },
-        ]
+        cond['$or'] = [{ name: { $regex: req.query.key, $options: 'i' } }];
       }
 
       if (req.query.email) {
-        cond['$or'] = [
-          { email: { $regex: req.query.email, $options: "i" } },
-        ]
+        cond['$or'] = [{ email: { $regex: req.query.email, $options: 'i' } }];
       }
 
       if (req.query.key && req.query.date) {
         cond['$or'] = [
-          { name: { $regex: req.query.key, $options: "i" } },
+          { name: { $regex: req.query.key, $options: 'i' } },
           { createdAt: { $gte: startDate, $lte: endDate } },
-        ]
+        ];
       }
 
       if (req.query.email && req.query.date) {
         cond['$or'] = [
-          { email: { $regex: req.query.email, $options: "i" } },
+          { email: { $regex: req.query.email, $options: 'i' } },
           { createdAt: { $gte: startDate, $lte: endDate } },
-        ]
+        ];
       }
 
       if (req.query.key && req.query.emai && req.query.date) {
         cond['$or'] = [
-          { name: { $regex: req.query.key, $options: "i" } },
-          { email: { $regex: req.query.email, $options: "i" } },
+          { name: { $regex: req.query.key, $options: 'i' } },
+          { email: { $regex: req.query.email, $options: 'i' } },
           { createdAt: { $gte: startDate, $lte: endDate } },
-        ]
+        ];
       }
-     const totalUsers = await User.countDocuments(cond);
+      const totalUsers = await User.countDocuments(cond);
       const totalPages = Math.ceil(totalUsers / limit);
-      const u = await User.find(cond, '-password').sort({createdAt: -1,});
-      return response.ok(res, { data: u, pagination: {
+      const u = await User.find(cond, '-password').sort({ createdAt: -1 });
+      return response.ok(res, {
+        data: u,
+        pagination: {
           totalUsers,
           totalPages,
           currentPage: page,
           itemsPerPage: limit,
-        }, });
+        },
+      });
     } catch (error) {
       return response.error(res, error);
     }
@@ -545,5 +559,4 @@ module.exports = {
   //     return response.error(res, error);
   //   }
   // },
-
 };
