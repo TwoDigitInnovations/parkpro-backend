5e42403f023363095897097b151e972d54aacb49
Rishabh-2digit <rishabhtiwari73096@gmail.com>
2026-05-20 12:05:52 +0530

    fix

--- src/routes/reportRoutes.js
+++ src/routes/reportRoutes.js
@@ -1,15 +1,52 @@
 const express = require('express');
 const router = express.Router();
-const { createReport, getReport, updateVerifyandSuspendStatus,getReportForGuard,getReportHistory,updatereportstatus,getReportforuser } = require('@controllers/reportController');
+const {
+  createReport,
+  getReport,
+  updateVerifyandSuspendStatus,
+  getReportForGuard,
+  getReportHistory,
+  updatereportstatus,
+  getReportforuser,
+} = require('@controllers/reportController');
 const auth = require('@middlewares/authMiddleware');
 const { upload } = require('@services/fileUpload');
 
-router.post('/createReport', auth('user', 'admin', 'guard', 'org', 'tech'),upload.single('image'), createReport);
-router.get('/getReport', auth('user', 'admin', 'guard', 'org', 'tech'), getReport);
-router.get('/getReportforuser', auth('user', 'admin', 'guard', 'org', 'tech'), getReportforuser);
-router.post('/getReportForGuard', auth('user', 'admin', 'guard', 'org', 'tech'), getReportForGuard);
-router.post('/updateVerifyandSuspendStatus', auth('user', 'admin', 'guard', 'org', 'tech'), updateVerifyandSuspendStatus);
-router.post('/updatereportstatus', auth('user', 'admin', 'guard', 'org', 'tech'), updatereportstatus);
-router.get('/getReportHistory', auth('user', 'admin', 'guard', 'org', 'tech'), getReportHistory);
+router.post(
+  '/createReport',
+  auth('user', 'admin', 'guard', 'org', 'tech', 'superadmin'),
+  upload.single('image'),
+  createReport,
+);
+router.get(
+  '/getReport',
+  auth('user', 'admin', 'guard', 'org', 'tech', 'superadmin'),
+  getReport,
+);
+router.get(
+  '/getReportforuser',
+  auth('user', 'admin', 'guard', 'org', 'tech', 'superadmin'),
+  getReportforuser,
+);
+router.post(
+  '/getReportForGuard',
+  auth('user', 'admin', 'guard', 'org', 'tech', 'superadmin'),
+  getReportForGuard,
+);
+router.post(
+  '/updateVerifyandSuspendStatus',
+  auth('user', 'admin', 'guard', 'org', 'tech', 'superadmin'),
+  updateVerifyandSuspendStatus,
+);
+router.post(
+  '/updatereportstatus',
+  auth('user', 'admin', 'guard', 'org', 'tech', 'superadmin'),
+  updatereportstatus,
+);
+router.get(
+  '/getReportHistory',
+  auth('user', 'admin', 'guard', 'org', 'tech', 'superadmin'),
+  getReportHistory,
+);
 
 module.exports = router;
